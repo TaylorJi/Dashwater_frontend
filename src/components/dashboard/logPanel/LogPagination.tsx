3b57c723dae61ef41fdf94af655a58ba558de835
susansli <susansemail195@gmail.com>
2023-05-01 10:40:17 -0700

    Move logs pagination component to its own file

---
+++ src/components/dashboard/logPanel/LogPagination.tsx
@@ -0,0 +1,159 @@
+import { Button, Center, Icon, Select } from '@chakra-ui/react';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect } from 'react';
+import uuid from 'react-uuid';
+import { useRecoilState, useRecoilValue, useResetRecoilState } from 'recoil';
+import colors from '../../../theme/foundations/colours';
+import { itemsPerPageAtom, logDataAtom, paginationMultipleAtom } from './atoms/logPanelAtoms';
+
+const LogPagination: React.FC = () => {
+
+    const logData = useRecoilValue(logDataAtom);
+    const [currPagination, setCurrPagination] = useRecoilState(paginationMultipleAtom);
+    const [itemsPerPage, setItemsPerPage] = useRecoilState(itemsPerPageAtom);
+
+    const resetPagination = useResetRecoilState(paginationMultipleAtom);
+    const resetItemsPerPage = useResetRecoilState(itemsPerPageAtom);
+
+    useEffect(() => {
+        return () => {
+            resetPagination();
+            resetItemsPerPage();
+        }
+    }, []);
+
+    return (
+        <>
+            {
+                logData &&
+                <Center
+                    mt='2rem'
+                >
+                    <Button
+                        size='sm'
+                        colorScheme='main'
+                        color='white'
+                        leftIcon={<Icon
+                            as={FontAwesomeIcon}
+                            icon={faChevronLeft}
+                            color='white'
+                        />}
+                        isDisabled={currPagination === 0}
+                        _hover={{
+                            bg: colors.main.activeMainButton
+                        }}
+                        onClick={() => {
+                            if (currPagination !== 0) {
+                                setCurrPagination(currPagination - 1);
+                            }
+                        }}
+                    >
+                        Prev Page
+                    </Button>
+
+                    <Select
+                        size='sm'
+                        mr='0.5rem'
+                        ml='1rem'
+                        w='20rem'
+                        borderRadius='0.25rem'
+                        borderColor={colors.main.usafaBlue}
+                        onChange={(e) => {
+                            setCurrPagination(parseInt(e.target.value));
+                        }}
+                        value={currPagination}
+                    >
+                        {
+                            logData.map((item, index) => {
+                                if ((index * itemsPerPage % itemsPerPage === 0)
+                                    && (index * itemsPerPage < logData.length)) {
+
+                                    // the last page
+                                    if ((index + 1) * itemsPerPage > logData.length) {
+                                        return (
+                                            <option
+                                                value={index}
+                                                key={uuid()}
+                                            >
+                                                Items {index * itemsPerPage + 1} to {logData.length}
+                                            </option>
+                                        );
+
+                                    } else {
+                                        return (
+                                            <option
+                                                value={index}
+                                                key={uuid()}
+                                            >
+                                                Items {index * itemsPerPage + 1} to {(index + 1) * itemsPerPage}
+                                            </option>
+                                        );
+                                    }
+                                }
+                            })
+                        }
+                    </Select>
+
+                    <Select
+                        size='sm'
+                        mr='1rem'
+                        w='10rem'
+                        borderRadius='0.25rem'
+                        borderColor={colors.main.usafaBlue}
+                        onChange={(e) => {
+                            resetPagination();
+                            setItemsPerPage(parseInt(e.target.value));
+                        }}
+                        value={itemsPerPage}
+                    >
+                        <option
+                            value={15}
+                            key={uuid()}
+                        >
+                            15 items per page
+                        </option>
+                        <option
+                            value={25}
+                            key={uuid()}
+                        >
+                            25 items per page
+                        </option>
+                        <option
+                            value={50}
+                            key={uuid()}
+                        >
+                            50 items per page
+                        </option>
+                    </Select>
+
+                    <Button
+                        size='sm'
+                        colorScheme='main'
+                        color='white'
+                        rightIcon={<Icon
+                            as={FontAwesomeIcon}
+                            icon={faChevronRight}
+                            color='white'
+                        />}
+                        isDisabled={(currPagination + 1) * itemsPerPage >= logData.length}
+                        _hover={{
+                            bg: colors.main.activeMainButton
+                        }}
+                        onClick={() => {
+                            if ((currPagination + 1) * itemsPerPage < logData.length) {
+                                setCurrPagination(currPagination + 1);
+                            }
+                        }}
+                    >
+                        Next Page
+                    </Button>
+                </Center>
+
+            }
+        </>
+
+    );
+};
+
+export default LogPagination;
--- src/components/dashboard/logPanel/Logs.tsx
+++ src/components/dashboard/logPanel/Logs.tsx
@@ -1,27 +1,15 @@
 import { createColumnHelper } from '@tanstack/react-table';
 import { BaseTable } from './BaseTable';
 import { mockLogData } from '../../../mockData/dashboardMockData';
-import { Box, Button, Center, Icon, Select, Text, useMediaQuery } from '@chakra-ui/react';
+import { Box, Text, useMediaQuery } from '@chakra-ui/react';
 import { useRecoilState, useRecoilValue, useResetRecoilState } from 'recoil';
-import {
-    displayedLogDataSelector, logDataAtom,
-    paginationMultipleAtom, itemsPerPageAtom
-} from './atoms/logPanelAtoms';
-import colors from '../../../theme/foundations/colours';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { displayedLogDataSelector, logDataAtom } from './atoms/logPanelAtoms';
 import { useEffect } from 'react';
-import uuid from 'react-uuid';
+import LogPagination from './LogPagination';
 
 const Logs: React.FC = () => {
-
-    const [itemsPerPage, setItemsPerPage] = useRecoilState(itemsPerPageAtom);
     const [logData, setLogData] = useRecoilState(logDataAtom);
     const displayedLogData = useRecoilValue(displayedLogDataSelector);
-    const [currPagination, setCurrPagination] = useRecoilState(paginationMultipleAtom);
-
-    const resetPagination = useResetRecoilState(paginationMultipleAtom);
-    const resetItemsPerPage = useResetRecoilState(itemsPerPageAtom);
 
     const [isLargeScreen] = useMediaQuery('(min-width: 1600px)');
     const columnHelper = createColumnHelper<logDataType>();
@@ -29,11 +17,6 @@ const Logs: React.FC = () => {
     useEffect(() => {
 
         setLogData(mockLogData);
-
-        return () => {
-            resetPagination();
-            resetItemsPerPage();
-        }
 
     }, []);
 
@@ -127,129 +110,8 @@ const Logs: React.FC = () => {
                         >
                             <BaseTable columns={columns} data={displayedLogData} />
                         </Box>
+                        <LogPagination />
 
-                        <Center
-                            mt='2rem'
-                        >
-                            <Button
-                                size='sm'
-                                colorScheme='main'
-                                color='white'
-                                leftIcon={<Icon
-                                    as={FontAwesomeIcon}
-                                    icon={faChevronLeft}
-                                    color='white'
-                                />}
-                                isDisabled={currPagination === 0}
-                                _hover={{
-                                    bg: colors.main.activeMainButton
-                                }}
-                                onClick={() => {
-                                    if (currPagination !== 0) {
-                                        setCurrPagination(currPagination - 1);
-                                    }
-                                }}
-                            >
-                                Prev Page
-                            </Button>
-
-                            <Select
-                                size='sm'
-                                mr='0.5rem'
-                                ml='1rem'
-                                w='20rem'
-                                borderRadius='0.25rem'
-                                borderColor={colors.main.usafaBlue}
-                                onChange={(e) => {
-                                    setCurrPagination(parseInt(e.target.value));
-                                }}
-                                value={currPagination}
-                            >
-                                {
-                                    logData.map((item, index) => {
-                                        if ((index * itemsPerPage % itemsPerPage === 0)
-                                            && (index * itemsPerPage < logData.length)) {
-
-                                            // the last page
-                                            if ((index + 1) * itemsPerPage > logData.length) {
-                                                return (
-                                                    <option
-                                                        value={index}
-                                                        key={uuid()}
-                                                    >
-                                                        Items {index * itemsPerPage + 1} to {logData.length}
-                                                    </option>
-                                                );
-
-                                            } else {
-                                                return (
-                                                    <option
-                                                        value={index}
-                                                        key={uuid()}
-                                                    >
-                                                        Items {index * itemsPerPage + 1} to {(index + 1) * itemsPerPage}
-                                                    </option>
-                                                );
-                                            }
-                                        }
-                                    })
-                                }
-                            </Select>
-
-                            <Select
-                                size='sm'
-                                mr='1rem'
-                                w='10rem'
-                                borderRadius='0.25rem'
-                                borderColor={colors.main.usafaBlue}
-                                onChange={(e) => {
-                                    resetPagination();
-                                    setItemsPerPage(parseInt(e.target.value));
-                                }}
-                                value={itemsPerPage}
-                            >
-                                <option
-                                    value={15}
-                                    key={uuid()}
-                                >
-                                    15 items per page
-                                </option>
-                                <option
-                                    value={25}
-                                    key={uuid()}
-                                >
-                                    25 items per page
-                                </option>
-                                <option
-                                    value={50}
-                                    key={uuid()}
-                                >
-                                    50 items per page
-                                </option>
-                            </Select>
-
-                            <Button
-                                size='sm'
-                                colorScheme='main'
-                                color='white'
-                                rightIcon={<Icon
-                                    as={FontAwesomeIcon}
-                                    icon={faChevronRight}
-                                    color='white'
-                                />}
-                                isDisabled={(currPagination + 1) * itemsPerPage >= logData.length}
-                                _hover={{
-                                    bg: colors.main.activeMainButton
-                                }}
-                                onClick={() => {
-                                    if ((currPagination + 1) * itemsPerPage < logData.length) {
-                                        setCurrPagination(currPagination + 1);
-                                    }
-                                }}
-                            >
-                                Next Page
-                            </Button>
-                        </Center>
                     </>
                     : 'Loading'
             }
