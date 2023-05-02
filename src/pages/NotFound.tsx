b3202a2edbecbf3873ace696f486c0e5c5d7114d
JacksonMars <jmarsnas@my.bcit.ca>
2023-05-02 08:22:01 -0700

    Completed 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import BaseLayout from '../components/layout/BaseLayout';
+import { Image, Button, Center, Text, VStack, useMediaQuery } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import colors from '../theme/foundations/colours';
 
 const NotFound: React.FC = () => {
+    const [isLargeScreen] = useMediaQuery('(min-width: 1600px)');
+    const navigate = useNavigate()
 
     return (
-        <BaseLayout isNavbarVisible={false}>
-            <></>
-        </BaseLayout>
+        <Center w='100vw'>
+            <VStack spacing={"30px"}>
+                <Image src="/404_graphic.svg" />
+
+                <Text fontSize={isLargeScreen ? '3xl' : '2xl'}>
+                    404: The requested page could not be found.
+                </Text>
+
+                <Button 
+                    size='lg'
+                    colorScheme='main'
+                    _hover={{
+                        bg: colors.main.ceruBlue
+                    }}
+                    onClick={() => {
+                        navigate('/dashboard');
+                    }}
+                >
+                    Return to Dashboard
+                </Button>
+            </VStack>
+        </Center>
     );
 };
 
