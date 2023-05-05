f7967878ece702d4acebb65fe2c65bda1f05864f
Sally Poon <sallypoon@hotmail.com>
2023-05-05 00:50:43 -0700

    Refactor calibration panel

--- src/components/deviceManager/DeviceSettingsModal/CalibrationSettingsPanel/CalibrationSettingsPanel.tsx
+++ src/components/deviceManager/DeviceSettingsModal/CalibrationSettingsPanel/CalibrationSettingsPanel.tsx
@@ -1,37 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import colors from '../../../../theme/foundations/colours';
 import {
-    Box,
-    Button,
     Divider,
     Text,
-    Flex,
-    Heading,
-    NumberInput,
-    NumberInputField,
-    AlertDialog,
-    AlertDialogOverlay,
-    AlertDialogHeader,
-    AlertDialogContent,
-    AlertDialogBody,
-    AlertDialogFooter,
-    useDisclosure,
     Select,
-    Popover,
-    PopoverTrigger,
-    Portal,
-    PopoverContent,
-    PopoverArrow,
-    PopoverCloseButton,
-    PopoverHeader,
-    PopoverBody,
-    PopoverFooter,
-    AlertDialogCloseButton,
 } from '@chakra-ui/react';
-import { toast } from 'react-hot-toast';
 import { buoySensorTags } from '../../../../theme/metrics/buoySensorTags';
 import uuid from 'react-uuid';
-import ManageDevices from '../../../../api/ManageDevices/ManageDevices';
 import CalibrationTable from './CalibrationTable';
 
 type calibrationSettingsPanelProps = {
@@ -39,34 +14,8 @@ type calibrationSettingsPanelProps = {
 }
 
 const CalibrationSettingsPanel: React.FC<calibrationSettingsPanelProps> = ({ sensors }) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const cancelRef = useRef(null);
-
-    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [currentMetric, setCurrentMetric] = useState<string>("");
-    const [calibrationInfo, setCalibrationInfo] = useState<sensorType>({} as sensorType);
-    const [unsavedChanges, setUnsavedChanges] = useState<boolean>(false);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (calibrationInfo) {
-            setUnsavedChanges(true);
-            setCalibrationInfo({ ...calibrationInfo, [e.target.name]: e.target.value });
-        }
-    };
-
-    const saveCalibrationPoint = async () => {
-        setIsLoading(true);
-        if (calibrationInfo) {
-            const res = await ManageDevices.saveCalibrationPoint(calibrationInfo);
-            if (res) {
-                toast.success('Calibration point saved!');
-            } else {
-                toast.error('There was a problem saving the calibration points. Please try again.')
-            }
-            setIsLoading(false);
-            setUnsavedChanges(false);
-        }
-    };
+    const [currentSensor, setCurrentSensor] = useState<sensorType>({} as sensorType);
 
     return (
         <>
@@ -81,11 +30,10 @@ const CalibrationSettingsPanel: React.FC<calibrationSettingsPanelProps> = ({ sen
                 value={currentMetric}
                 borderColor={colors.main.usafaBlue}
                 onChange={e => {
-
                     let sensor = sensors.find(sensor => sensor.metric_type === e.target.value)
                     if (sensor) {
                         setCurrentMetric(e.target.value);
-                        setCalibrationInfo(sensor);
+                        setCurrentSensor(sensor);
                     }
                 }}
             >
@@ -119,86 +67,9 @@ const CalibrationSettingsPanel: React.FC<calibrationSettingsPanelProps> = ({ sen
             {
                 currentMetric !== "" &&
                 <CalibrationTable
-                    calibrationPoints={
-                        calibrationInfo.calibration_points
-                    }
-                    unit={calibrationInfo.default_metric_unit}
-                    setUnsavedChanges={setUnsavedChanges}
+                    sensor={currentSensor}
                 />
             }
-
-
-            <Flex mt={8} justifyContent="flex-end">
-                <Button
-                    mr={1.5}
-                    border='1px'
-                    borderColor={colors.main.usafaBlue}
-                    color={colors.main.usafaBlue}
-                    bg='transparent'
-                    isLoading={isLoading}
-                    onClick={onOpen}
-                    _hover={{
-                        bg: colors.main.ceruBlue
-                    }}
-                    loadingText='Saving'
-                    isDisabled={!unsavedChanges}
-                >
-                    Revert Changes
-                </Button>
-
-
-                <Button
-                    bg={colors.main.usafaBlue}
-                    color='white'
-                    isLoading={isLoading}
-                    onClick={onOpen}
-                    _hover={{
-                        bg: colors.main.ceruBlue
-                    }}
-                    loadingText='Saving'
-                    isDisabled={currentMetric == ""}
-                >
-                    Calibrate
-                </Button>
-            </Flex>
-
-            <AlertDialog
-                isOpen={isOpen}
-                leastDestructiveRef={cancelRef}
-                onClose={onClose}
-                variant="flushed"
-                isCentered
-            >
-
-                <AlertDialogOverlay>
-                    <AlertDialogContent>
-                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-                            Save Calibration Points?
-                        </AlertDialogHeader>
-                        <AlertDialogCloseButton />
-
-                        <AlertDialogBody>
-                            Are you sure you want to save these calibration points? This will overwrite previous calibration point values.
-                        </AlertDialogBody>
-
-                        <AlertDialogFooter>
-                            <Button ref={cancelRef} onClick={onClose}>
-                                Cancel
-                            </Button>
-                            <Button
-                                bg={colors.main.usafaBlue}
-                                color='white'
-                                _hover={{
-                                    bg: colors.main.ceruBlue
-                                }}
-                                onClick={async () => await saveCalibrationPoint()} 
-                                ml={3}>
-                                Confirm Calibration
-                            </Button>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialogOverlay>
-            </AlertDialog>
         </>
     );
 };
--- src/components/deviceManager/DeviceSettingsModal/CalibrationSettingsPanel/CalibrationTable.tsx
+++ src/components/deviceManager/DeviceSettingsModal/CalibrationSettingsPanel/CalibrationTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import colors from '../../../../theme/foundations/colours';
 import {
     Table,
@@ -6,16 +6,54 @@ import {
     Tbody,
     Tr,
     Th,
+    Flex,
+    Button,
+    AlertDialog,
+    AlertDialogOverlay,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogBody,
+    AlertDialogFooter,
+    useDisclosure,
+    AlertDialogCloseButton,
 } from '@chakra-ui/react';
+import { toast } from 'react-hot-toast';
 import CalibrationPointRow from './CalibrationPointRow';
+import ManageDevices from '../../../../api/ManageDevices/ManageDevices';
+
 
 type calibrationTableProp = {
-    calibrationPoints: calibrationPointType[];
-    unit: string;
-    setUnsavedChanges: React.Dispatch<React.SetStateAction<boolean>>;
+    sensor: sensorType;
 }
 
-const CalibrationTable: React.FC<calibrationTableProp> = ({ calibrationPoints, unit, setUnsavedChanges }) => {
+const CalibrationTable: React.FC<calibrationTableProp> = ({ sensor }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = useRef(null);
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [unsavedChanges, setUnsavedChanges] = useState<boolean>(false);
+    const [sensorInfo, setSensorInfo] = useState<sensorType>({} as sensorType);
+
+    const saveCalibrationPoint = async () => {
+        setIsLoading(true);
+        const res = await ManageDevices.saveCalibrationPoint(sensorInfo);
+        if (res) {
+            toast.success('Calibration point saved!');
+        } else {
+            toast.error('There was a problem saving the calibration points. Please try again.')
+        }
+        setIsLoading(false);
+        setUnsavedChanges(false);
+    };
+
+    const handleChange = (point: calibrationPointType) => { 
+        setSensorInfo({ ... sensor, calibration_points: sensor.calibration_points.map(p => {
+            if (p.id === point.id) {
+                return point;
+            }
+            return p;
+        })});
+    };
 
     return (
         <>
@@ -30,25 +68,95 @@ const CalibrationTable: React.FC<calibrationTableProp> = ({ calibrationPoints, u
                     </Tr>
                 </Thead>
                 <Tbody>
-                    {calibrationPoints.length > 0 ?
-                        calibrationPoints.sort((a, b) => {
-                            return a.id - b.id;
-                        }).map((point, index) => {
-                            return (
-                                <CalibrationPointRow
-                                    number={index + 1}
-                                    point={point}
-                                    unit={unit}
-                                    setUnsavedChanges={setUnsavedChanges}
-                                    key={index}
-                                />
-                            )
-                        }) : <></>
+                    {
+                        sensor['calibration_points'].length > 0 ?
+                            sensor['calibration_points'].sort((a, b) => {
+                                return a.id - b.id;
+                            }).map((point, index) => {
+                                return (
+                                    <CalibrationPointRow
+                                        number={index + 1}
+                                        point={point}
+                                        unit={sensor.default_metric_unit}
+                                        setUnsavedChanges={setUnsavedChanges}
+                                        key={index}
+                                    />
+                                )
+                            }) : <></>
                     }
                 </Tbody>
             </Table>
 
-            
+            <Flex mt={8} justifyContent="flex-end">
+                <Button
+                    mr={1.5}
+                    border='1px'
+                    borderColor={colors.main.usafaBlue}
+                    color={colors.main.usafaBlue}
+                    bg='transparent'
+                    isLoading={isLoading}
+                    onClick={onOpen}
+                    _hover={{
+                        bg: colors.main.ceruBlue
+                    }}
+                    loadingText='Saving'
+                    isDisabled={!unsavedChanges}
+                >
+                    Revert Changes
+                </Button>
+
+
+                <Button
+                    bg={colors.main.usafaBlue}
+                    color='white'
+                    isLoading={isLoading}
+                    onClick={onOpen}
+                    _hover={{
+                        bg: colors.main.ceruBlue
+                    }}
+                    loadingText='Saving'
+                >
+                    Calibrate
+                </Button>
+            </Flex>
+
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+                variant="flushed"
+                isCentered
+            >
+
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            Save Calibration Points?
+                        </AlertDialogHeader>
+                        <AlertDialogCloseButton />
+
+                        <AlertDialogBody>
+                            Are you sure you want to save these calibration points? This will overwrite previous calibration point values.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button
+                                bg={colors.main.usafaBlue}
+                                color='white'
+                                _hover={{
+                                    bg: colors.main.ceruBlue
+                                }}
+                                onClick={async () => await saveCalibrationPoint()}
+                                ml={3}>
+                                Confirm Calibration
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
         </>
     );
 }
