ee869c8f68bd58067ce96743718c2c02c722f084
taylorJ <taylor.ji719@gmail.com>
2023-05-08 02:13:45 -0700

    implemented basic create function on the front-end

--- src/api/AdminPortal/AdminPortal.ts
+++ src/api/AdminPortal/AdminPortal.ts
@@ -1,5 +1,14 @@
 import axios, { AxiosResponse } from "axios";
+import { type } from "os";
+import { useNavigate } from "react-router-dom";
 import { API_URL } from "../Environments";
+
+
+type createUserResponse = {
+    email: string,
+    password: string,
+    role: string;
+}
 
 //20230505 EJ - created AdminPortal.ts
 const getUser = async () => {
@@ -19,9 +28,23 @@ const getUser = async () => {
     }
 };
 
-const createUser = async () => {
 
-};
+const createUser = async () => {
+    // show up the new component allowing admin to create a new user 
+    // currently hardcoded test user account create when create button is clicked 
+    // update email, password part 
+    console.log("create is clicked")
+    const request: any = await axios.post<createUserResponse>(`${API_URL}/user/createUser`,
+    {email: "test23@test.ca", password: "testPass1#", role: "User"}, 
+    {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      },);
+    console.log(request)
+    }
+      
 
 const deleteUser = async (idArray: string[]) => {
     console.log(idArray);
