2c7b62b284d602f2cd30972c9281e717689b38e0
Sally Poon <sallypoon@hotmail.com>
2023-05-17 16:23:38 -0700

    function to fetch device settings data from BE

--- src/api/ManageDevices/ManageDevices.ts
+++ src/api/ManageDevices/ManageDevices.ts
@@ -1,3 +1,6 @@
+import axios, { AxiosResponse } from "axios";
+import { API_URL } from "../Environments";
+
 const saveDeviceSettings = async (settings: generalSettingsType) => {
     // do DB stuff in try-catch block
     return true;
@@ -7,23 +10,36 @@ const saveDeviceSettings = async (settings: generalSettingsType) => {
 const saveThresholdSettings = async () => {
     // do DB stuff in try-catch block
     return true;
-}
+};
 
 const saveCalibrationPoint = async (calibrationInfo: sensorType) => {
     // do DB stuff for the metric type
     return true;
-}
+};
 
 const removePreviousCalibration = async () => {
     // do DB stuff. should likely return the previous previous calibration points.
     return true;
-}
+};
+
+const getDevicesSettings = async () => {
+    try {
+        const response: any = await axios.get(`${API_URL}/device/getAllDevicesSettings`, { withCredentials: true });
+        if (response.status === 200) {
+            console.log(response.data.data);
+            return response.data.data;
+        }
+    } catch (_err) {
+        return null;
+    }
+};
 
 const ManageDevices = {
     saveDeviceSettings,
     saveThresholdSettings,
     saveCalibrationPoint,
-    removePreviousCalibration
+    removePreviousCalibration,
+    getDevicesSettings
 };
 
 export default ManageDevices;
--- src/components/wrappers/DeviceDetailsWrapper/DeviceDetailsWrapper.tsx
+++ src/components/wrappers/DeviceDetailsWrapper/DeviceDetailsWrapper.tsx
@@ -1,31 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import axios from 'axios';
 import { allDevicesDetails } from './deviceManagerAtoms';
+import { toast } from 'react-hot-toast';
 import mockBuoyData from '../../../mockData/mockBuoyData.json';
+import ManageDevices from '../../../api/ManageDevices/ManageDevices';
 
 type deviceDetailsWrapperProps = {
     children?: React.ReactNode;
 }
 
 const DeviceDetailsWrapper: React.FC<deviceDetailsWrapperProps> = ({ children }) => {
+
     const setDevicesDetails = useRecoilState(allDevicesDetails)[1];
+
+    const fetchData = async () => {
+
+        try {
+            const data = await ManageDevices.getDevicesSettings();
+            if (data) {
+                setDevicesDetails(data);
+            } else {
+                toast.error('There was an error fetching device data - please refresh and try again.');
+            }
+
+        } catch(_err) {
+
+            console.log(_err)
+            return null;
+
+        }
+    }
 
     
     useEffect(() => {
-
-        const fetchData = async () => {
-            // TO DO: replace with actual API call
-            // const res = await axios.get('./mockData/mockBuoyData.json');
-            // return res.data.buoys;
-
-            return mockBuoyData.buoys;
-        }
-        
-        fetchData().then(data => {
-            setDevicesDetails(data);
-        });
-
+        fetchData();
         return () => {
             // cleanup
         };
