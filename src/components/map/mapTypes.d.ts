477ab020193e04ac61131af0ba7c7e8806ab3f33
Basil Kim <80417893+Basil-Kim@users.noreply.github.com>
2023-05-17 17:14:54 -0700

    Reformat file and remove comments

--- src/components/map/mapTypes.d.ts
+++ src/components/map/mapTypes.d.ts
@@ -7,23 +7,16 @@ type buoyMapInfo = {
   }[];
 };
 
-
-
-//TODO:This will need to be replaced
 type buoyInfo = {
   name: string;
   id: number;
   x: number;
   y: number;
-  }[];
-
-
+}[];
 
 type deviceMapInfo = {
   name: string;
   id: string;
   x: number;
   y: number;
-}
-
-
+};
