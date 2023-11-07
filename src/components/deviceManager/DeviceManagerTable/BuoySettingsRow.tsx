f5c5a3c97290fd0a362726445cf9acd180429198
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2023-11-06 16:46:44 -0800

    visualise sensor data in manageDevice page

--- src/components/deviceManager/DeviceManagerTable/BuoySettingsRow.tsx
+++ src/components/deviceManager/DeviceManagerTable/BuoySettingsRow.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import uuid from 'react-uuid';
-import { 
-    Tr, 
+import {
+    Tr,
     Td,
     Text
 } from '@chakra-ui/react';
@@ -14,27 +14,25 @@ type buoySettingsRowProps = {
 
 
 const BuoySettingsRow: React.FC<buoySettingsRowProps> = ({ buoy }) => {
-
     return (
-            <Tr h='6rem'>
-                <Td><Text as='b'>{buoy.name}</Text></Td>
-                <Td>{buoy.id}</Td>
-                <Td>
-                    {
-                        buoy['sensors'].map(sensor => {
-                            console.log(sensor.metric);
-                            return (
-                                <SensorTag metricKey={sensor.metric} key={uuid()} />
-                            );
-                        })
-                    }
-                </Td>
-                <Td>
-                    <DeviceSettingsModal
-                        buoy={buoy}
-                    />
-                </Td>
-            </Tr>
+        <Tr h='6rem'>
+            <Td><Text as='b'>{buoy.name}</Text></Td>
+            <Td>{buoy.id}</Td>
+            <Td>
+                {
+                    buoy['sensors'].map(sensor => {
+                        return (
+                            <SensorTag metricKey={sensor.metric} key={uuid()} />
+                        );
+                    })
+                }
+            </Td>
+            <Td>
+                <DeviceSettingsModal
+                    buoy={buoy}
+                />
+            </Td>
+        </Tr>
     );
 };
 
