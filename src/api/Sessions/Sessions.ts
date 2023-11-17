c5eb70ff6158ce7fc87674c908b70cdc36d19153
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2023-11-16 21:19:36 -0800

    working on sessions

--- src/api/Sessions/Sessions.ts
+++ src/api/Sessions/Sessions.ts
@@ -18,7 +18,32 @@ const validateSession = async () => {
         console.log(response);
 
         if (response.status === 200) {
-            return response.data.user;
+            const returnedSessionId = response.data.sessionId;
+            var sessionArray: any[] = JSON.parse(localStorage.getItem("sessionArray")!);
+            let foundSession = sessionArray.find(session => session.sessionId === returnedSessionId);
+            const expirationTime = new Date();
+            expirationTime.setHours(expirationTime.getHours() + 2);
+            foundSession.sessionExpiry = expirationTime.toISOString();
+            localStorage.setItem("sessionArray", JSON.stringify(sessionArray));
+            const res = await axios.post(`${API_URL}/session/updateSessionExpiry`, 
+            {
+                newSession: foundSession,
+                withCredenitials: true
+            });
+            if (res.status === 200) {
+                const res1: any = await axios.get<any, AxiosResponse<string>>(`https://ma93xudga3.execute-api.us-east-1.amazonaws.com/prod/data/?email=${foundSession.userId}`)
+                if (res1.status === 200) {
+                    const fetchedUser = res1.data;
+                    const user: userDataType = {
+                        email: fetchedUser["email"],
+                        // userId: fetchedUser["_id"], 
+                        userId: fetchedUser["email"],
+                        role: fetchedUser["role"],
+                    };
+                    return user;
+                }
+            }
+            // return response.data.user;
         }
 
         return null;
@@ -40,6 +65,15 @@ const createSession = async (userId: string, idToken: string, userRole: string) 
         
 
         if (response.status === 200) {
+            if (localStorage.getItem("sessionArray")) {
+                var sessionArray: any[] = JSON.parse(localStorage.getItem("sessionArray")!);
+                sessionArray.push(response.data.user);
+                localStorage.setItem("sessionArray", JSON.stringify(sessionArray));
+            } else {
+                var sessionArray: any[] = [];
+                sessionArray.push(response.data.user);
+                localStorage.setItem("sessionArray", JSON.stringify(sessionArray));
+            }
             return response.data.user;
         }
 
@@ -62,6 +96,9 @@ const deleteSession = async () => {
             });
 
         if (response.status === 200) {
+            var sessionArray: any[] = JSON.parse(localStorage.getItem("sessionArray")!);
+            sessionArray = sessionArray.filter(session => session.sessionId !== response.data.sessionId);
+            localStorage.setItem("sessionArray", JSON.stringify(sessionArray));
             return true;
         }
 
