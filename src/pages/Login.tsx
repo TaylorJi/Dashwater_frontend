4804b2b99f253aa31305019473e59baab3a04053
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2023-11-30 15:27:38 -0800

    login changed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -46,10 +46,8 @@ import ManageDevices from "../api/ManageDevices/ManageDevices";
 
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState("admin-testing@gmail.com");
-  const [password, setPassword] = useState("Testing123!");
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
   const navigate = useNavigate();
